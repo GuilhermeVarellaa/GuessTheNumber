fcf848939ecd661fb92646f83b86dfba9773689a
Guilherme Varella <guilhermeraul1@hotmail.com>
2025-05-05 11:10:51 -0300

    feat: finaliza jogo de adivinhação com verificação e tentativas

--- src/JogoAdivinhação.java
+++ src/JogoAdivinhação.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class JogoAdivinhação {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
+        int numeroSecreto = random.nextInt(100) + 1;
+        int tentativas = 0;
+        boolean sucesso = false;
 
+        System.out.println("Bem vindo ao GuessTheNumber");
+        System.out.println("Tente advinhar o numero entre 1 e 100");
+
+        while(!sucesso){
+            System.out.println("Digite seu palpite: ");
+            int palpite = scanner.nextInt();
+            tentativas ++;
+
+            if (palpite > numeroSecreto){
+                System.out.println("Você chutou muito alto.");
+            } else if (palpite < numeroSecreto) {
+                System.out.println("Você chutou muito baixo.");
+            }else {
+                sucesso = true;
+                System.out.println("Você acertou!!! Você acertou com " + tentativas + " tentativas");
+            }
         }
+
     }
+}
